2d67aed6d6df31ecde3be1e6f015a8d03dbc8c32
sage-lyon <sage_lyon@student.uml.edu>
2022-02-11 23:04:03 -0500

    Cleaned up code and fixed some errors

--- main.c
+++ main.c
@@ -7,14 +7,15 @@
 #define MAX_CHILDREN 25
 
 int main (int argc, char** argv) {
+
     pid_t child_pid;
-    int child_number, num_children;
     pid_t pid_list[MAX_CHILDREN];
+    int num_children;
 
     // Check if number of arguments is correct
     if(argc != 2){
         fprintf(stderr, "ERROR: Invalid number of arguments.\n");
-	return 1;
+	    return 1;
     }
 
     // Check if argument between 0 and the maximum ammount of children allowed.
@@ -26,49 +27,58 @@ int main (int argc, char** argv) {
         return 1;
     }
     
+    // Print parent PID
     printf("Parent pid is %d\n", getpid());
 
+    // Loop to create children processes
     for(int i = 0; i < num_children; i++){
+
+	    // Create new child process
         child_pid = fork();
+
+	    // If the process is a child process, execute a test program based on the order of its creation
         if(child_pid == 0){
-            child_number = i;
-            switch(child_number % 5) {
+            switch(i % 5) {
                 case 0:
-                    execlp("./test1", "");
+                    execlp("./test1", NULL);
                     break;
 
                 case 1:
-                    execlp("./test2", "");
+                    execlp("./test2", NULL);
                     break;
 
                 case 2:
-                    execlp("./test3", "");
+                    execlp("./test3", NULL);
                     break;
 
                 case 3:
-                    execlp("./test4", "");
+                    execlp("./test4", NULL);
                     break;
 
                 case 4:
-                    execlp("./test5", "");
+                    execlp("./test5", NULL);
                     break;
             }
-            break;
+	        break;
         }
+
+	    // If process is the parent process, add the newly created child's pid to the child pid list
         else {
             pid_list[i] = child_pid;
         }
     }
-    if(child_pid != 0){
-        for(int i = 0; i < num_children; i++){
-            child_pid = wait(NULL);
-            for(int j = 0; j < num_children; j++){
-                if(pid_list[j] == child_pid){
-                    printf("Child %d (PID = %d) finished \n", j + 1, child_pid);
-                }
-            }
-        }  
-    }
 
+    // Loop to wait for all children processes to finish execution
+    for(int i = 0; i < num_children; i++){
+
+	    child_pid = wait(NULL);
+
+	    // Once child has completed it's execution, loop through pid list to find what # child it is and print that it has finished
+        for(int j = 0; j < num_children; j++){
+            if(pid_list[j] == child_pid){
+	            printf("Child %d (PID = %d) finished \n", j + 1, child_pid);
+	        }
+	    }
+    }  
     return 0;
 }
